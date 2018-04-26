f3f0c68dc9f57a8b5aaa1af242285a01a5e87931
Mircea <mircea.livadariu@gmail.com>
2018-04-25 22:11:53 -0700

    tools_install script

--- bin/tools_install.sh
+++ bin/tools_install.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+oldstate=$(set +o)
+set -euxo pipefail
+
 SELFDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOPDIR=$(dirname $SELFDIR)
+
 echo "Topdir is "$TOPDIR
+VENVDIR=$TOPDIR"/tools/venv"
+CFGDIR=$TOPDIR"/tools/cfg"
+
+rm -rf $VENVDIR/*
+virtualenv $VENVDIR
+cp $CFGDIR"/requirements.txt" $VENVDIR"/"
+eval "$oldstate"
+source $VENVDIR/bin/activate
+cd $VENVDIR && pip install -r requirements.txt
+deactivate
