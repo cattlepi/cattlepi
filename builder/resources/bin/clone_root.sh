462b6e2896fcdb53aea5347748c434ba2b4ec444
Mircea <mircea.livadariu@gmail.com>
2018-05-12 14:48:50 -0700

    dump the rsync into a separate sh script to bypass an the yaml parsing, shell escaping funkiness also attempt to run the fetch w/ become=false to bypass using slurp and blowing up the memory used

---
+++ builder/resources/bin/clone_root.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# this could be in the ansible playbook, but the yaml parser + escaping makes it just easier to put here/sync/execute
+#
+sudo rsync -qaHAXS --exclude={"/boot/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} / /tmp/squashfs/rootfs
