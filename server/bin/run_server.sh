5cfb779d68e2399247b64aa7548c31dbdcfaba5e
Mircea <mircea.livadariu@gmail.com>
2018-06-21 22:15:42 -0700

    update run_server w/ dev ip

--- server/bin/run_server.sh
+++ server/bin/run_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SELFDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export SERVERIP=192.168.0.1 # the ip which the server will listen to
+export SERVERIP=192.168.1.166 # the ip which the server will listen to
 cd $SELFDIR && cd .. && bundle exec rake && bundle exec ruby lib/server.rb
