1840fa2e3e81f1c71171da9c98df2c35f4619330
Mircea <mircea.livadariu@gmail.com>
2018-08-05 21:07:42 -0700

    add gunicorn + start of the new server

---
+++ server/server.py
@@ -0,0 +1,21 @@
+import falcon
+
+class DeviceConfigResource(object):
+    def on_get(self, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.body = "Ok"
+
+class ImagesResource(object):
+    def on_get(self, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.body = "Ok"
+
+class TrackResource(object):
+    def on_get(self, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.body = "Ok"
+
+app = falcon.API()
+app.add_route('/boot/{deviceid}/config', DeviceConfigResource())
+app.add_route('/images/{space}/{filename}',ImagesResource())
+app.add_route('/track', TrackResource())
