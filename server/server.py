1e428e6e4963293b09553cf396b1031aff48b265
Mircea <mircea.livadariu@gmail.com>
2018-08-05 21:13:36 -0700

    update the track resources

--- server/server.py
+++ server/server.py
@@ -1,11 +1,16 @@
 import falcon
 
 class DeviceConfigResource(object):
-    def on_get(self, req, resp):
+    def on_get(self, req, resp, deviceid):
         resp.status = falcon.HTTP_200
         resp.body = "Ok"
 
 class ImagesResource(object):
+    def on_get(self, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.body = "Ok"
+
+class TrackAllResource(object):
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
         resp.body = "Ok"
@@ -18,4 +23,5 @@ class TrackResource(object):
 app = falcon.API()
 app.add_route('/boot/{deviceid}/config', DeviceConfigResource())
 app.add_route('/images/{space}/{filename}',ImagesResource())
-app.add_route('/track', TrackResource())
+app.add_route('/track', TrackAllResource())
+app.add_route('/track/{deviceid}', TrackResource())
