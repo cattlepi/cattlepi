0c356fb4eff3b0125163a8aadbcd12f09b56ef95
Mircea <hello@cattlepi.com>
2018-08-31 22:20:07 -0700

    add a reboot jitter (random sleep) to prevent all devices from going down at the same time

--- templates/raspbian/resources/bin/autoupdate.sh
+++ templates/raspbian/resources/bin/autoupdate.sh
@@ -12,9 +12,14 @@ CONFIG_REL=$(/usr/bin/head -1 /cattlepi/base_relative_config)
 MATCH=0
 /usr/bin/cmp -s /tmp/current_config /cattlepi/config || MATCH=1
 if [ $MATCH -ne 0 ]; then
+    # add up to 550 seconds delay before rebooting (ie sleep(random(550)) in bash)
+    #   this should help in theory if you have a large number of devices 
+    #   and you are driving all of them off the default config 
+    sleep $(( RANDOM %= 550 ))
     set +e 
     /sbin/reboot -f 
-    if [ $? -ne 0]; then
+    if [ $? -ne 0 ]; then
+        # the reboot did not work. force it even more
         echo 1 > /proc/sys/kernel/sysrq
         echo b > /proc/sysrq-trigger        
     fi
