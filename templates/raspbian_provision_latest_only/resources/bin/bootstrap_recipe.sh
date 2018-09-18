ba9500f5b2a1eab6d92f7cd6eb0bbe2a8c466867
Mircea <hello@cattlepi.com>
2018-09-17 22:29:50 -0700

    add sync

--- templates/raspbian_provision_latest_only/resources/bin/bootstrap_recipe.sh
+++ templates/raspbian_provision_latest_only/resources/bin/bootstrap_recipe.sh
@@ -71,9 +71,17 @@ NEW_PTUUID=$(blkid /dev/mmcblk0 | sed -r 's/.+PTUUID="([^"]+).+/\1/')
 sed -ri "s/$OLD_PTUUID/$NEW_PTUUID/" /p1/cmdline.txt
 sed -ri "s/$OLD_PTUUID/$NEW_PTUUID/" /p2/etc/fstab
 
+# debug output
+echo "++++++++"
+find /tmp/boot -print
+cat /p1/cmdline.txt
+echo "++++++++"
+
 # we're done. force restart
+sync
 umount /p1
 umount /tmp/boot
 umount /p2
+sync
 sleep 5
 echo b > /proc/sysrq-trigger
