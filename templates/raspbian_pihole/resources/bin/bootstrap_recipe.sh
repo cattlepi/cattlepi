11d93c92d4994a900d9041cc29ddad39ae14fb57
Mircea <hello@cattlepi.com>
2018-09-30 21:21:02 -0700

    add bootstrap_recipe script to fix the ip + network and actually make cattlepi-pihole work

---
+++ templates/raspbian_pihole/resources/bin/bootstrap_recipe.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# prepare the replacements
+source /etc/pihole/setupVars.conf
+OLD_IPV4CIDR=$IPV4_ADDRESS
+OLD_IPV4IP=$(echo $OLD_IPV4CIDR | cut -d '/' -f 1)
+OLD_IFACE=$PIHOLE_INTERFACE
+NEW_IPV4CIDR=$(/etc/pihole/detect_ip.sh)
+NEW_IPV4IP=$(echo $NEW_IPV4CIDR | cut -d '/' -f 1)
+NEW_IFACE=$(/etc/pihole/detect_network.sh)
+
+## replacements
+sed -i 's/'"$OLD_IPV4IP"'/'"$NEW_IPV4IP"'/g' /etc/pihole/local.list
+sed -i 's/'"$OLD_IPV4CIDR"'/'"$NEW_IPV4CIDR"'/g' /etc/pihole/setupVars.conf
+sed -i 's/'"$OLD_IFACE"'/'"$NEW_IFACE"'/g' /etc/pihole/setupVars.conf
+sed -i 's/'"$OLD_IFACE"'/'"$NEW_IFACE"'/g' /etc/dnsmasq.d/01-pihole.conf
+
+## restart
+/etc/init.d/pihole-FTL restart
