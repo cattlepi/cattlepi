ed501a3398c8715fe1610ae49362c54622134792
Mircea <hello@cattlepi.com>
2018-09-30 22:10:48 -0700

    replace / with | to make the CIDR substitution work

--- templates/raspbian_pihole/resources/bin/bootstrap_recipe.sh
+++ templates/raspbian_pihole/resources/bin/bootstrap_recipe.sh
@@ -9,10 +9,10 @@ NEW_IPV4IP=$(echo $NEW_IPV4CIDR | cut -d '/' -f 1)
 NEW_IFACE=$(/etc/pihole/detect_network.sh)
 
 ## replacements
-sed -i 's/'"$OLD_IPV4IP"'/'"$NEW_IPV4IP"'/g' /etc/pihole/local.list
-sed -i 's/'"$OLD_IPV4CIDR"'/'"$NEW_IPV4CIDR"'/g' /etc/pihole/setupVars.conf
-sed -i 's/'"$OLD_IFACE"'/'"$NEW_IFACE"'/g' /etc/pihole/setupVars.conf
-sed -i 's/'"$OLD_IFACE"'/'"$NEW_IFACE"'/g' /etc/dnsmasq.d/01-pihole.conf
+sed -i 's|'"$OLD_IPV4IP"'|'"$NEW_IPV4IP"'|g' /etc/pihole/local.list
+sed -i 's|'"$OLD_IPV4CIDR"'|'"$NEW_IPV4CIDR"'|g' /etc/pihole/setupVars.conf
+sed -i 's|'"$OLD_IFACE"'|'"$NEW_IFACE"'|g' /etc/pihole/setupVars.conf
+sed -i 's|'"$OLD_IFACE"'|'"$NEW_IFACE"'|g' /etc/dnsmasq.d/01-pihole.conf
 
 ## restart
 /etc/init.d/pihole-FTL restart
