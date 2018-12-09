fdd2ead1181dd3acc054e24d0e16aad992fa0e1b
Mircea <hello@cattlepi.com>
2018-12-09 09:41:38 -0800

    update ansible cfg

--- tools/util/run_playbook.sh
+++ tools/util/run_playbook.sh
@@ -4,9 +4,6 @@ if [ "$#" -ne 3 ]; then
     exit 2
 else
     source $TOPDIR/bin/"activate"
-    export ANSIBLE_DEBUG=True
-    mkdir -p ${BUILDDIRLATEST}/output
-    export ANSIBLE_LOG_PATH=${BUILDDIRLATEST}/output/ansible.log
-    cd $1 && ansible-playbook -vvvv -i $2 $3
+    cd $1 && ansible-playbook -vv -i $2 $3
     deactivate
 fi
