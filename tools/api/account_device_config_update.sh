22c5a799264d396d7b1a49e7c570487857bd63eb
Mircea <hello@cattlepi.com>
2019-02-09 22:10:32 -0800

    add pre/post hooks setup to be able to hook this into automation

--- tools/api/account_device_config_update.sh
+++ tools/api/account_device_config_update.sh
@@ -6,6 +6,10 @@ ARG_APIKEY="NONE"
 ARG_INCREMENTAL=0
 ARG_DEVICE="default"
 
+# lifecycle hooks
+ARG_H_BEFORE="NONE"
+ARG_H_AFTER="NONE"
+
 # payload arguments
 ARG_P_BOOTCODE="NONE"
 ARG_P_USERCODE="NONE"
@@ -30,6 +34,14 @@ while (( "$#" )); do
       ARG_INCREMENTAL=1
       shift 1
       ;;
+    -hkb|--hook-before)
+      ARG_H_BEFORE=$2
+      shift 2
+      ;;
+    -hka|--hook-after)
+      ARG_H_AFTER=$2
+      shift 2
+      ;;
     -pb|--payload-bootcode)
       ARG_P_BOOTCODE=$2
       shift 2
@@ -41,6 +53,10 @@ while (( "$#" )); do
     -pca|--payload-config-autoupdate)
       ARG_P_C_AUTOUPDATE=1
       shift 1
+      ;;
+    -pcs|--payload-config-sdlayout)
+      ARG_P_C_SDLAYOUT=$2
+      shift 2
       ;;
     --) # end argument parsing
       shift
@@ -59,6 +75,20 @@ done
 if [ "$ARG_APIKEY" == "NONE" ]; then
     echo "Error: Expecting api key" >&2
     exit 1
+fi
+
+# run the before lifecycle hook
+if [ "$ARG_H_BEFORE" != "NONE" ]; then
+  if [ -x "$ARG_H_BEFORE" ]; then
+    $ARG_H_BEFORE
+    if [ $? -ne 0 ]; then
+      echo "Error: Failed executing the _before_ lifecycle hook" >&2
+      exit 1
+    fi
+  else
+      echo "Error: Cannot execute _before_ lifecycle hook" >&2
+      exit 1
+  fi
 fi
 
 # incremental means that we take the config we currently have and just update element in it
@@ -107,6 +137,20 @@ if [ "$ARG_P_C_AUTOUPDATE" -eq 1 ]; then
   BASE_CONFIG=$(echo "$BASE_CONFIG" | jq '.config.autoupdate=true')
 else
   BASE_CONFIG=$(echo "$BASE_CONFIG" | jq '.config.autoupdate=false')
+fi
+
+# sdlayout
+if [ "$ARG_P_C_SDLAYOUT" == "WIPE" ]; then
+  BASE_CONFIG=$(echo "$BASE_CONFIG" | jq 'del(.config.sdlayout)')
+else
+  if [ "$ARG_P_C_SDLAYOUT" != "NONE" ]; then
+    if [ ! -r "$ARG_P_C_SDLAYOUT" ]; then
+      echo "Error: Cannot read file with sdlayout ("$ARG_P_C_SDLAYOUT")" >&2
+      exit 1
+    fi
+    export SDLAYOUT=$(cat "$ARG_P_C_SDLAYOUT" | base64 -w 0)
+    BASE_CONFIG=$(echo "$BASE_CONFIG" | jq '.config.sdlayout=env.SDLAYOUT')
+  fi
 fi
 
 echo $BASE_CONFIG | jq
